e7e28dba8771fb84cd18ce5af954226e247c397d
Fazztech-AI <fazzolari.j@hotmail.com>
2026-06-29 09:02:42 +1000

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,13 +1,19 @@
 from stores import jbhifi
+from stores import mock_store
 from database import save_price_check
 
 def search_prices(query):
     results = []
 
+    results.extend(mock_store.search(query))
     results.extend(jbhifi.search(query))
 
     saved_results = []
     for result in results:
         saved_results.append(save_price_check(result))
+
+    saved_results.sort(
+        key=lambda x: x["price"] if x["price"] is not None else 999999
+    )
 
     return saved_results
