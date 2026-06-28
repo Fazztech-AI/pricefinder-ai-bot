e215fc9c59d7cdc6eba64bae7cd6482a1b44b24e
Fazztech-AI <fazzolari.j@hotmail.com>
2026-06-29 08:58:53 +1000

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,8 +1,13 @@
 from stores import jbhifi
+from database import save_price_check
 
 def search_prices(query):
     results = []
 
     results.extend(jbhifi.search(query))
 
-    return results
+    saved_results = []
+    for result in results:
+        saved_results.append(save_price_check(result))
+
+    return saved_results
